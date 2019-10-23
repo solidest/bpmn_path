f7aff22ba910e58a249f74e1ac2090cd510180d7
solidest <solidest@163.com>
2019-10-23 13:01:06 +0800

    update

--- src/LevelPath.js
+++ src/LevelPath.js
@@ -5,6 +5,9 @@ function createLevelPath(graph, depth) {
     let path_results = [
         [graph.start_node]
     ];
+    if(depth<1) {
+        return path_results;
+    }
 
     console.log("待完成...");
     
